cfce2f68d5cc3b662adedab9fa7cce5f5382338e
aluczynska <79922121+aluczynska@users.noreply.github.com>
2021-05-18 11:32:35 +0200

    Update autocompleter.js

--- google/autocompleter.js
+++ google/autocompleter.js
@@ -1,19 +1,9 @@
 Vue.component('v-autocompleter', {
-  <div class="lista">
-    <input ref="first" v-model="googleSearch" type="text" class="glownainput" @focus="focused" v-on:keyup.down="downKey" v-on:keyup.up="upKey" v-on:keyup.enter="enterKey"/>
-      <div class="bottom_border"></div>	
-         <div class="list">	
-             <ul v-for="(city, index) in filteredCities" v-on:click="update_input(city.name)">	
-              <li :class="{greyspace: index == in_focus}">		
-              <a v-on:click="choose(index)" v-html="bolderize(city)">{{ city }}</a>	
-                    </li>	
-                  </ul>	
-                </div>	
-              </div>	
-              
-    var app = new Vue({	
-    el: '#app',	
-    data: {	
+
+  props: ['options'],
+
+  data: function () {
+    return {
       in_focus: -1,	
       focused: true,
       selected_city: '',	
@@ -106,8 +96,19 @@ Vue.component('v-autocompleter', {
       input_city.name.replace(regex, match =>	
           {return "<span class='thin'>"+ match +"</span>";}) 	
               + "</b>";	
-      return bold;	
-  }	
-  }	
-});
-              })
+      return bold;
+}
+
+
+},
+
+    templete:'<input ref="first" v-model="googleSearch" type="text" class="glownainput" @focus="focused" v-on:keyup.down="downKey" v-on:keyup.up="upKey" v-on:keyup.enter="enterKey"/>\
+                    <div class="bottom_border"></div>\	
+                <div class="list">\	
+                  <ul v-for="(city, index) in filteredCities" v-on:click="update_input(city.name)">\	
+                    <li :class="{greyspace: index == in_focus}">\		
+                       <a v-on:click="choose(index)" v-html="bolderize(city)">{{ city }}</a>\
+                      </li>\
+                    </ul>\
+                  </div>'
+  })
